6fb35cb50cb61d3bfb691334015b828c86661011
Kimonae <70099141+Kimonae@users.noreply.github.com>
2023-02-03 14:43:09 +0100

    add php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wild Code project</title>
+    <meta name="author" content="D.">
+    <meta name="description" content="Voici un projet demandé par la formation, dont la principale utilité est de montrer que je sais manier du sql ">
+    <link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+
+<header> </header>
+<h1> Galerie d'images</h1>
+
+
+<div class="gal"></div>
+
+<?php
+require_once("connexion.php");
+
+?>
+
+
+    
+</body>
+</html>
